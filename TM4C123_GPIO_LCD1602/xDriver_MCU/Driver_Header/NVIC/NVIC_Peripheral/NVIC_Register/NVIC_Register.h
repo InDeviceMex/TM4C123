0ba3d838749df0621fee0cca0c5ea2f3a667f862
InDeviceMex <indevicemex@gmail.com>
2020-06-22 02:34:22 -0500

    Actualizar NVIC_Register.h, NVIC_RegisterDefines.h y 35 más archivos...

--- TM4C123_GPIO_LCD1602/xDriver_MCU/Driver_Header/NVIC/NVIC_Peripheral/NVIC_Register/NVIC_Register.h
+++ TM4C123_GPIO_LCD1602/xDriver_MCU/Driver_Header/NVIC/NVIC_Peripheral/NVIC_Register/NVIC_Register.h
@@ -10,7 +10,7 @@
 
 
 
-#include <xDriver_MCU/Driver_Header/NVIC/NVIC_Peripheral/NVIC_Register/NVIC_RegisterDefines.h>
+#include <xDriver_MCU/Driver_Header/NVIC/NVIC_Peripheral/NVIC_Register/NVIC_RegisterDefines/NVIC_RegisterDefines.h>
 #include <xDriver_MCU/Driver_Header/NVIC/NVIC_Peripheral/NVIC_Register/NVIC_RegisterPeripheral.h>
 
 #endif /* XDRIVER_MCU_DRIVER_HEADER_NVIC_NVIC_PERIPHERAL_NVIC_REGISTER_H_ */
---
+++ TM4C123_GPIO_LCD1602/xDriver_MCU/Driver_Header/NVIC/NVIC_Peripheral/NVIC_Register/NVIC_RegisterDefines/NVIC_RegisterDefines_Set/NVIC_RegisterDefines_Set0/NVIC_RegisterDefines_Set0_ICPR.h
@@ -0,0 +1,388 @@
+/**
+ *
+ * @file NVIC_RegisterDefines_Set0_ICPR.h
+ * @copyright
+ * @verbatim InDeviceMex 2020 @endverbatim
+ *
+ * @par Responsibility
+ * @verbatim InDeviceMex Developers @endverbatim
+ *
+ * @version
+ * @verbatim 1.0 @endverbatim
+ *
+ * @date
+ * @verbatim 21 jun. 2020 @endverbatim
+ *
+ * @author
+ * @verbatim vyldram @endverbatim
+ *
+ * @par Change History
+ * @verbatim
+ * Date           Author     Version     Description
+ * 21 jun. 2020     vyldram    1.0         initial Version@endverbatim
+ */
+#ifndef XDRIVER_MCU_DRIVER_HEADER_NVIC_NVIC_PERIPHERAL_NVIC_REGISTER_NVIC_REGISTERDEFINES_NVIC_REGISTERDEFINES_SET0_NVIC_REGISTERDEFINES_SET0_ICPR_H_
+#define XDRIVER_MCU_DRIVER_HEADER_NVIC_NVIC_PERIPHERAL_NVIC_REGISTER_NVIC_REGISTERDEFINES_NVIC_REGISTERDEFINES_SET0_NVIC_REGISTERDEFINES_SET0_ICPR_H_
+
+/********************************************************************************************/
+/************************************* ICPR0 ***********************************************/
+/********************************************************************************************/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_GPIOA_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_R_NVIC_GPIOA_BIT     (0u)
+#define NVIC_ICPR0_R_NVIC_GPIOA_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOA_EN      ((uint32_t)0x00000001u)
+
+#define NVIC_ICPR0_NVIC_GPIOA_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_GPIOA_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_GPIOA_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_GPIOB_MASK    ((uint32_t)0x00000002u)
+#define NVIC_ICPR0_R_NVIC_GPIOB_BIT     (1u)
+#define NVIC_ICPR0_R_NVIC_GPIOB_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOB_EN      ((uint32_t)0x00000002u)
+
+#define NVIC_ICPR0_NVIC_GPIOB_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_GPIOB_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_GPIOB_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_GPIOC_MASK    ((uint32_t)0x00000004u)
+#define NVIC_ICPR0_R_NVIC_GPIOC_BIT     (2u)
+#define NVIC_ICPR0_R_NVIC_GPIOC_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOC_EN      ((uint32_t)0x00000004u)
+
+#define NVIC_ICPR0_NVIC_GPIOC_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_GPIOC_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_GPIOC_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_GPIOD_MASK    ((uint32_t)0x00000008u)
+#define NVIC_ICPR0_R_NVIC_GPIOD_BIT     (3u)
+#define NVIC_ICPR0_R_NVIC_GPIOD_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOD_EN      ((uint32_t)0x00000008u)
+
+#define NVIC_ICPR0_NVIC_GPIOD_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_GPIOD_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_GPIOD_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_GPIOE_MASK    ((uint32_t)0x00000010u)
+#define NVIC_ICPR0_R_NVIC_GPIOE_BIT     (4u)
+#define NVIC_ICPR0_R_NVIC_GPIOE_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOE_EN      ((uint32_t)0x00000010u)
+
+#define NVIC_ICPR0_NVIC_GPIOE_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_GPIOE_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_GPIOE_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_UART0_MASK    ((uint32_t)0x00000020u)
+#define NVIC_ICPR0_R_NVIC_UART0_BIT     (5u)
+#define NVIC_ICPR0_R_NVIC_UART0_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_UART0_EN      ((uint32_t)0x00000020u)
+
+#define NVIC_ICPR0_NVIC_UART0_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_UART0_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_UART0_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_UART1_MASK    ((uint32_t)0x00000040u)
+#define NVIC_ICPR0_R_NVIC_UART1_BIT     (6u)
+#define NVIC_ICPR0_R_NVIC_UART1_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_UART1_EN      ((uint32_t)0x00000040u)
+
+#define NVIC_ICPR0_NVIC_UART1_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_UART1_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_UART1_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_SSI0_MASK     ((uint32_t)0x00000080u)
+#define NVIC_ICPR0_R_NVIC_SSI0_BIT      (7u)
+#define NVIC_ICPR0_R_NVIC_SSI0_DIS      ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_SSI0_EN       ((uint32_t)0x00000080u)
+
+#define NVIC_ICPR0_NVIC_SSI0_MASK       ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_SSI0_DIS        ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_SSI0_EN         ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_I2C0_MASK     ((uint32_t)0x00000100u)
+#define NVIC_ICPR0_R_NVIC_I2C0_BIT      (8u)
+#define NVIC_ICPR0_R_NVIC_I2C0_DIS      ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_I2C0_EN       ((uint32_t)0x00000100u)
+
+#define NVIC_ICPR0_NVIC_I2C0_MASK       ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_I2C0_DIS        ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_I2C0_EN         ((uint32_t)0x00000001u)
+/*----------*/
+
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_PWM0FAULT_MASK ((uint32_t)0x00000200u)
+#define NVIC_ICPR0_R_NVIC_PWM0FAULT_BIT  (9u)
+#define NVIC_ICPR0_R_NVIC_PWM0FAULT_DIS  ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_PWM0FAULT_EN   ((uint32_t)0x00000200u)
+
+#define NVIC_ICPR0_NVIC_PWM0FAULT_MASK   ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_PWM0FAULT_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_PWM0FAULT_EN     ((uint32_t)0x00000001u)
+/*----------*/
+
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_PWM0GEN0_MASK  ((uint32_t)0x00000400u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN0_BIT   (10u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN0_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN0_EN    ((uint32_t)0x00000400u)
+
+#define NVIC_ICPR0_NVIC_PWM0GEN0_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_PWM0GEN0_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_PWM0GEN0_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_PWM0GEN1_MASK  ((uint32_t)0x00000800u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN1_BIT   (11u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN1_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN1_EN    ((uint32_t)0x00000800u)
+
+#define NVIC_ICPR0_NVIC_PWM0GEN1_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_PWM0GEN1_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_PWM0GEN1_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_PWM0GEN2_MASK  ((uint32_t)0x00001000u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN2_BIT   (12u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN2_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_PWM0GEN2_EN    ((uint32_t)0x00001000u)
+
+#define NVIC_ICPR0_NVIC_PWM0GEN2_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_PWM0GEN2_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_PWM0GEN2_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_QEI0_MASK     ((uint32_t)0x00002000u)
+#define NVIC_ICPR0_R_NVIC_QEI0_BIT      (13u)
+#define NVIC_ICPR0_R_NVIC_QEI0_DIS      ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_QEI0_EN       ((uint32_t)0x00002000u)
+
+#define NVIC_ICPR0_NVIC_QEI0_MASK       ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_QEI0_DIS        ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_QEI0_EN         ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ0_MASK ((uint32_t)0x00004000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ0_BIT  (14u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ0_DIS  ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ0_EN   ((uint32_t)0x00004000u)
+
+#define NVIC_ICPR0_NVIC_ADC0SEQ0_MASK   ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ0_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ0_EN ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ1_MASK ((uint32_t)0x00008000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ1_BIT  (15u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ1_DIS  ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ1_EN   ((uint32_t)0x00008000u)
+
+#define NVIC_ICPR0_NVIC_ADC0SEQ1_MASK   ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ1_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ1_EN     ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ2_MASK ((uint32_t)0x00010000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ2_BIT  (16u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ2_DIS  ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ2_EN   ((uint32_t)0x00010000u)
+
+#define NVIC_ICPR0_NVIC_ADC0SEQ2_MASK   ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ2_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ2_EN ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ3_MASK ((uint32_t)0x00020000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ3_BIT  (17u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ3_DIS  ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_ADC0SEQ3_EN   ((uint32_t)0x00020000u)
+
+#define NVIC_ICPR0_NVIC_ADC0SEQ3_MASK   ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ3_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_ADC0SEQ3_EN     ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_WDT01_MASK    ((uint32_t)0x00040000u)
+#define NVIC_ICPR0_R_NVIC_WDT01_BIT     (18u)
+#define NVIC_ICPR0_R_NVIC_WDT01_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_WDT01_EN      ((uint32_t)0x00040000u)
+
+#define NVIC_ICPR0_NVIC_WDT01_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_WDT01_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_WDT01_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_TIMER0A_MASK  ((uint32_t)0x00080000u)
+#define NVIC_ICPR0_R_NVIC_TIMER0A_BIT   (19u)
+#define NVIC_ICPR0_R_NVIC_TIMER0A_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER0A_EN    ((uint32_t)0x00080000u)
+
+#define NVIC_ICPR0_NVIC_TIMER0A_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_TIMER0A_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_TIMER0A_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_TIMER0B_MASK  ((uint32_t)0x00100000u)
+#define NVIC_ICPR0_R_NVIC_TIMER0B_BIT   (20u)
+#define NVIC_ICPR0_R_NVIC_TIMER0B_DI    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER0B_EN    ((uint32_t)0x00100000u)
+
+#define NVIC_ICPR0_NVIC_TIMER0B_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_TIMER0B_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_TIMER0B_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_TIMER1A_MASK  ((uint32_t)0x00200000u)
+#define NVIC_ICPR0_R_NVIC_TIMER1A_BIT   (21u)
+#define NVIC_ICPR0_R_NVIC_TIMER1A_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER1A_EN    ((uint32_t)0x00200000u)
+
+#define NVIC_ICPR0_NVIC_TIMER1A_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_TIMER1A_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_TIMER1A_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_TIMER1B_MASK  ((uint32_t)0x00400000u)
+#define NVIC_ICPR0_R_NVIC_TIMER1B_BIT   (22u)
+#define NVIC_ICPR0_R_NVIC_TIMER1B_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER1B_EN    ((uint32_t)0x00400000u)
+
+#define NVIC_ICPR0_NVIC_TIMER1B_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_TIMER1B_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_TIMER1B_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_TIMER2A_MASK  ((uint32_t)0x00800000u)
+#define NVIC_ICPR0_R_NVIC_TIMER2A_BIT   (23u)
+#define NVIC_ICPR0_R_NVIC_TIMER2A_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER2A_EN    ((uint32_t)0x00800000u)
+
+#define NVIC_ICPR0_NVIC_TIMER2A_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_TIMER2A_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_TIMER2A_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_TIMER2B_MASK  ((uint32_t)0x01000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER2B_BIT   (24u)
+#define NVIC_ICPR0_R_NVIC_TIMER2B_DIS   ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_TIMER2B_EN    ((uint32_t)0x01000000u)
+
+#define NVIC_ICPR0_NVIC_TIMER2B_MASK    ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_TIMER2B_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_TIMER2B_EN      ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_ACOMP0_MASK   ((uint32_t)0x02000000u)
+#define NVIC_ICPR0_R_NVIC_ACOMP0_BIT    (25u)
+#define NVIC_ICPR0_R_NVIC_ACOMP0_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_ACOMP0_EN     ((uint32_t)0x02000000u)
+
+#define NVIC_ICPR0_NVIC_ACOMP0_MASK     ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_ACOMP0_DIS      ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_ACOMP0_EN       ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_ACOMP1_MASK   ((uint32_t)0x04000000u)
+#define NVIC_ICPR0_R_NVIC_ACOMP1_BIT    (26u)
+#define NVIC_ICPR0_R_NVIC_ACOMP1_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_ACOMP1_EN     ((uint32_t)0x04000000u)
+
+#define NVIC_ICPR0_NVIC_ACOMP1_MASK     ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_ACOMP1_DIS      ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_ACOMP1_EN       ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_RES43_MASK    ((uint32_t)0x08000000u)
+#define NVIC_ICPR0_R_NVIC_RES43_BIT     (27u)
+#define NVIC_ICPR0_R_NVIC_RES43_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_RES43_EN      ((uint32_t)0x08000000u)
+
+#define NVIC_ICPR0_NVIC_RES43_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_RES43_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_RES43_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_SYSCTL_MASK   ((uint32_t)0x10000000u)
+#define NVIC_ICPR0_R_NVIC_SYSCTL_BIT    (28u)
+#define NVIC_ICPR0_R_NVIC_SYSCTL_DIS    ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_SYSCTL_EN     ((uint32_t)0x10000000u)
+
+#define NVIC_ICPR0_NVIC_SYSCTL_MASK     ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_SYSCTL_DIS      ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_SYSCTL_EN       ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_FLASH_MASK    ((uint32_t)0x20000000u)
+#define NVIC_ICPR0_R_NVIC_FLASH_BIT     (29u)
+#define NVIC_ICPR0_R_NVIC_FLASH_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_FLASH_EN      ((uint32_t)0x20000000u)
+
+#define NVIC_ICPR0_NVIC_FLASH_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_FLASH_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_FLASH_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_GPIOF_MASK    ((uint32_t)0x40000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOF_BIT     (30u)
+#define NVIC_ICPR0_R_NVIC_GPIOF_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_GPIOF_EN      ((uint32_t)0x40000000u)
+
+#define NVIC_ICPR0_NVIC_GPIOF_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_GPIOF_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_GPIOF_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+/*----------*/
+#define NVIC_ICPR0_R_NVIC_RES47_MASK    ((uint32_t)0x80000000u)
+#define NVIC_ICPR0_R_NVIC_RES47_BIT     (31u)
+#define NVIC_ICPR0_R_NVIC_RES47_DIS     ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_R_NVIC_RES47_EN      ((uint32_t)0x80000000u)
+
+#define NVIC_ICPR0_NVIC_RES47_MASK      ((uint32_t)0x00000001u)
+#define NVIC_ICPR0_NVIC_RES47_DIS       ((uint32_t)0x00000000u)
+#define NVIC_ICPR0_NVIC_RES47_EN        ((uint32_t)0x00000001u)
+/*----------*/
+
+#endif /* XDRIVER_MCU_DRIVER_HEADER_NVIC_NVIC_PERIPHERAL_NVIC_REGISTER_NVIC_REGISTERDEFINES_NVIC_REGISTERDEFINES_SET0_NVIC_REGISTERDEFINES_SET0_ICPR_H_ */
