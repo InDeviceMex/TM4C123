354914d07ca5af68faccdba11a635271ab618329
InDeviceMex <indevicemex@gmail.com>
2020-07-19 13:17:28 -0500

    [TM4C123_GPIO_LCD1602] Intial revision WDT driver

---
+++ TM4C123_GPIO_LCD1602/xDriver_MCU/Driver_Header/WDT/WDT.h
@@ -0,0 +1,31 @@
+/**
+ *
+ * @file WDT.h
+ * @copyright
+ * @verbatim InDeviceMex 2020 @endverbatim
+ *
+ * @par Responsibility
+ * @verbatim InDeviceMex Developers @endverbatim
+ *
+ * @version
+ * @verbatim 1.0 @endverbatim
+ *
+ * @date
+ * @verbatim 19 jul. 2020 @endverbatim
+ *
+ * @author
+ * @verbatim vyldram @endverbatim
+ *
+ * @par Change History
+ * @verbatim
+ * Date           Author     Version     Description
+ * 19 jul. 2020     vyldram    1.0         initial Version@endverbatim
+ */
+#ifndef XDRIVER_MCU_DRIVER_HEADER_WDT_WDT_H_
+#define XDRIVER_MCU_DRIVER_HEADER_WDT_WDT_H_
+
+
+
+
+
+#endif /* XDRIVER_MCU_DRIVER_HEADER_WDT_WDT_H_ */
